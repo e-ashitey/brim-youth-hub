3123a07ff7cad149280edfe7b9a6369830202df2
e-ashitey <eaashitey39@gmail.com>
2025-05-05 15:11:37 +0000

    Implement GET endpoint for fetching user data by ID

---
+++ app/api/members/[id]/route.ts
@@ -0,0 +1,12 @@
+import { getUserById } from "@/lib/actions"
+import { NextResponse } from "next/server"
+
+export async function GET({ params }: { params: { id: string } }) {
+    const userData = await getUserById(params.id);
+
+    if (!userData) {
+        return NextResponse.json({ error: "Member not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(userData);
+}
