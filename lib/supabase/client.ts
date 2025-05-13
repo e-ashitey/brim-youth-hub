17cd0bfb48d8dfaf567d76ee9ab5d69f88712aa0
e-ashitey <eaashitey39@gmail.com>
2025-05-13 18:08:26 +0000

    feat: add supabase browser client for improved authentication handling

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,9 +1,12 @@
 import { createClient as createSupabaseClient } from "@supabase/supabase-js"
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs"
+
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createSupabaseClient(supabaseUrl, supabaseAnonKey)
+export const supabaseBrowserClient = createPagesBrowserClient({ supabaseUrl, supabaseKey: supabaseAnonKey })
 
 // export function createClient() {
 //   return createSupabaseClient(supabaseUrl, supabaseAnonKey)
